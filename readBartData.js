d513c54852c9c90cad650717ce85e1bf2372544b
Mark B <markallenblanchard@gmail.com>
2022-04-04 00:48:04 -0500

    fix bugs

--- readBartData.js
+++ readBartData.js
@@ -15,6 +15,7 @@ const defaultState = {
   previousParticipant: 0,
   pop: 0,
   cashIn: 0,
+  cashInOnset: 0,
   previousTokens: 0,
   reactionTime: -1,
 };
@@ -28,6 +29,7 @@ let state = {
   previousParticipant: 0,
   pop: 0,
   cashIn: 0,
+  cashInOnset: 0,
   previousTokens: 0,
   reactionTime: -1,
 };
@@ -58,14 +60,14 @@ const generateSummaryRow = (
   tokens,
   onset
 ) => {
-  const cashInOrPop = cashIn || pop;
+  const cashInOrPop = state.cashIn || state.pop;
   const duration = onset - state.pressOnset;
   const tokensGained = tokens - state.previousTokens;
 
   const summaryData = new Map();
-  summaryData.set('participant', participant);
-  summaryData.set('trial', trial);
-  summaryData.set('cashIn', cashIn);
+  summaryData.set('participant', state.previousParticipant);
+  summaryData.set('trial', state.previousTrial);
+  summaryData.set('cashIn', state.cashIn);
   summaryData.set('pop', state.pop);
   summaryData.set('totalTokens', cashInOrPop ? tokens : 'NA');
   summaryData.set('pressCount', state.pressCount);
@@ -74,14 +76,6 @@ const generateSummaryRow = (
   summaryData.set('reactionTime', state.reactionTime);
 
   return Array.from(summaryData, ([name, value]) => value).join(',');
-};
-
-const resetState = (state, defaultState, participant, trial, pop, cashIn) => {
-  state = { ...defaultState };
-  state.previousParticipant = participant;
-  state.previousTrial = trial;
-  state.pop = pop;
-  state.cashIn = cashIn;
 };
 
 fs.readdir('./bart_data/', (err, files) => {
@@ -96,12 +90,8 @@ fs.readdir('./bart_data/', (err, files) => {
       .pipe(csv())
       .on('data', (bartEvent) => {
         let {
-          X,
           Participant,
-          Run,
           Trial,
-          NewBalloon,
-          Winner,
           PressPump,
           ReleasePump,
           Tokens,
@@ -112,7 +102,6 @@ fs.readdir('./bart_data/', (err, files) => {
         } = bartEvent;
 
         // cast some bart event data to number type
-        X = +X;
         Condition = +Condition;
         Tokens = +Tokens;
         Onset = +Onset;
@@ -123,20 +112,70 @@ fs.readdir('./bart_data/', (err, files) => {
         ReleasePump = +ReleasePump;
         Participant = +Participant;
 
+        // Trial should never be 0
+        if (!Trial) return
+
         // if condition not a number, do not return
         if (!isNaN(Condition) && Condition > 0) return;
 
         // validate same trial has no cash in nor pop
-        if (state.previousTrial === Trial && (state.pop || state.cashIn)) {
+        if (state.previousTrial === Trial && state.cashIn) {
           return;
+        }
+        if (state.previousTrial === Trial && state.pop) {
+          return;
+        }
+
+        if (state.previousTrial !== Trial || state.previousParticipant !== Participant) {
+          // resetState(state, defaultState, Participant, Trial, Pop, CashIn);
+          state = { ...defaultState };
+          state.previousParticipant = Participant;
+          state.previousTrial = Trial;
         }
 
         // check if cash-in or pop
         if (CashIn) state.cashIn = CashIn;
         if (Pop) state.pop = Pop;
 
-        if (state.previousTrial !== Trial || state.previousParticipant !== Participant)
-          resetState(state, defaultState, Participant, Trial, Pop, CashIn);
+        if (state.cashIn) {
+          // should a cash in or pop have a reaction time ?
+          state.buttonStatus  = buttonStata.release;
+          state.cashInOnset = Onset;
+          state.reactionTime = state.cashInOnset - state.releaseOnset
+
+          summaryRows.push(
+            generateSummaryRow(
+              state,
+              state.previousparticipant,
+              state.previoustrial,
+              state.cashIn,
+              state.pop,
+              Tokens,
+              Onset
+            )
+          );
+          return
+        }
+
+        if (state.pop) {
+          // should a cash in or pop have a reaction time ?
+          state.buttonStatus = buttonStata.release;
+          state.cashInOnset = Onset;
+          state.reactionTime = 'NA'
+
+          summaryRows.push(
+            generateSummaryRow(
+              state,
+              state.previousparticipant,
+              state.previoustrial,
+              state.cashIn,
+              state.pop,
+              Tokens,
+              Onset
+            )
+          );
+          return
+        }
 
         if (state.buttonStatus === buttonStata.release) {
           if (PressPump === 1) {
@@ -144,47 +183,29 @@ fs.readdir('./bart_data/', (err, files) => {
             state.buttonStatus = buttonStata.press;
             state.pressOnset = Onset;
             state.previousTokens = Tokens;
-            state.pressCount += 1;
             state.reactionTime = Onset - state.releaseOnset;
           }
-        } else if (ReleasePump === 1) {
+          return;
+        }
+
+        if (state.buttonStatus === buttonStata.press && ReleasePump === 1) {
           // if this row recorded a release!
           state.buttonStatus = buttonStata.release;
           state.releaseOnset = Onset;
+          state.pressCount += 1;
 
           summaryRows.push(
             generateSummaryRow(
               state,
-              Participant,
-              Trial,
-              CashIn,
-              Pop,
+              state.previousparticipant,
+              state.previoustrial,
+              state.cashIn,
+              state.pop,
               Tokens,
               Onset
             )
           );
           return;
-        }
-        
-        if (CashIn === 1 || Pop === 1) {
-          // should a cash in or pop have a reaction time ?
-          state.buttonStatus = buttonStata.release;
-          state.releaseOnset = Onset;
-          state.reactionTime = CashIn
-            ? state.releaseOnset - state.pressOnset
-            : 'NA';
-
-          summaryRows.push(
-            generateSummaryRow(
-              state,
-              Participant,
-              Trial,
-              CashIn,
-              Pop,
-              Tokens,
-              Onset
-            )
-          );
         }
       })
       .on('end', () => {
